bafb10d4879ffc184c8d69a2890d1566955645ca
Daniel Waltrip <dwaltrip77@gmail.com>
2025-09-27 14:20:06 -0700

    wippp - working on FE chat actions

--- frontend/src/chat/actions.ts
+++ frontend/src/chat/actions.ts
@@ -1,14 +1,52 @@
+import type { ChatMessage } from "../../../common/src/types/db";
+import type { useChatStore } from "../pages/chat-page/chat-store";
+import { getSystemWsEffects, type SystemWsEffects } from "../system";
+import { getOrCreateWsClient } from "../ws/create-client";
+import { getChatWsEffects, type ChatWsEffects } from "./ws-effects";
 
-function addNewMessage(text: string, roomId: string, userId: string) {
-  console.log('[ChatClientActions] message received', { text, roomId, userId });
+interface ChatActionsDeps {
+  chatStore: typeof useChatStore;
+  chatWsEffects: ChatWsEffects;
+  systemWsEffects: SystemWsEffects;
 }
 
-function updateMessageWithEdits(messageId: string, newText: string) {
-  console.log('[ChatClientActions] message edited', { messageId, newText });
+interface ChatActions {
+  addNewMessage: (chatMsg: ChatMessage) => void;
+  joinRoom: (roomId: string) => void;
+  // updateIsTypingStatus: (roomId: string, userId: string, isTyping: boolean) => void;
 }
 
-function updateIsTypingStatus(roomId: string, userId: string, isTyping: boolean) {
-  console.log('[ChatClientActions] typing status', { roomId, userId, isTyping });
+function createChatActions(
+  { chatStore, chatWsEffects, systemWsEffects }: ChatActionsDeps 
+): ChatActions {
+  return {
+    addNewMessage(chatMsg: ChatMessage) {
+      console.log('[ChatClientActions] message received', chatMsg);
+      chatStore.getState().addMessage(chatMsg);
+    },
+
+    joinRoom(roomId: string) {
+      systemWsEffects.joinRoom(roomId);
+    }
+  }
 }
 
-export { addNewMessage, updateMessageWithEdits, updateIsTypingStatus };
+const getChatActions = (function() {
+  let actions = null as ChatActions | null;
+
+  function getChatActions(getChatStore: () => typeof useChatStore): ChatActions {
+    const client = getOrCreateWsClient();
+    if (!actions) {
+      actions = createChatActions({
+        chatStore: getChatStore(),
+        chatWsEffects: getChatWsEffects(client),
+        systemWsEffects: getSystemWsEffects(client),
+      });
+    }
+    return actions;
+  }
+  return getChatActions;
+})();
+
+export type { ChatActions };
+export { createChatActions, getChatActions };
